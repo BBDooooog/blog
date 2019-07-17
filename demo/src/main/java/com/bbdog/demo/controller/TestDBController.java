71ef20582c1648bffa9e6ead4afa907d602968e2
吴罡 <gang.wu@autozi.com>
2019-07-17 14:54:34 +0800

    学习@requeseParam和@requestBody

--- demo/src/main/java/com/bbdog/demo/controller/TestDBController.java
+++ demo/src/main/java/com/bbdog/demo/controller/TestDBController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/test")
 public class TestDBController {
@@ -37,6 +40,20 @@ public class TestDBController {
     public String getRedis(){
         redisUtils.set("a",1);
         return redisUtils.get("a").toString();
+    }
+
+    @RequestMapping("/testRequestParam")
+    public void requestParam(User user ,@RequestParam Integer userId,
+                            @RequestParam String userName,@RequestParam String password,@RequestParam String phone){
+        System.out.println("userId=" + userId + "userName=" + userName + "password=" + password + "phone="+ phone);
+        System.out.println(user.toString());
+    }
+
+    @RequestMapping("/testRequestBody")
+    public void requestBody(@RequestBody List<User> userList){
+        for (User user :userList){
+            System.out.println(user.toString());
+        }
     }
 
     @RequestMapping(value = "/getParam",method = RequestMethod.GET)
